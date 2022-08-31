179a49946ec470946b900bcd60a87ae257591fd7
tictac <fominsartems947@gmail.com>
2022-08-31 23:15:11 +0300

    s

--- src/components/hooks/getItems.ts
+++ src/components/hooks/getItems.ts
@@ -6,21 +6,21 @@ export const useGetAllItems = () => {
     const [isError,setError] = useState<boolean>(false);
     const [cartItems,setCartItems] = useState<ICartItem[]>([]);
     useEffect(()=>{
-        const request = async ():Promise<void> => {
-          setLoading(true)
-          try{
+      const request = async ():Promise<void> => {
+        setLoading(true)
+        try{
             const response = await fetch("https://61785c79bb979200171ff6cc.mockapi.io/sneakers");
             const json = await response.json();
             setCartItems(json);
             setLoading(false)
-          }
-          catch(e) {
+            }
+        catch(e) {
             console.log(e);
             setLoading(false);
             setError(true)
-          }
         }
-        request();
+          }
+          request();
       },[])
     return {
         isLoading,
